18eef41e3ea28439a5e8365b6d67309f2b4b74fa
uyedaj <juyeda@vt.edu>
2017-12-21 15:04:55 -0500

    fiddled with priors

--- R/bayouAnalysisMaximumSpeed.R
+++ R/bayouAnalysisMaximumSpeed.R
@@ -13,32 +13,32 @@ td <- filter(td, !is.na(LnMass))
 tree <- td$phy
 tree$edge.length[tree$edge.length==0] <- .Machine$double.eps
 dat <- log(td[['TempTraitValue']])
-pred <- select(td$dat, LnMass)#, Environment, TrophicGroup, Thermy)
+pred <- select(td$dat, LnMass, Environment, TrophicGroup, Thermy)
 
 prior.11 <- make.prior(tree, plot.prior = FALSE, 
-                       dists=list(dalpha="dhalfcauchy", dsig2="dhalfcauchy", dbeta_LnMass="dnorm",
+                       dists=list(dalpha="dlnorm", dsig2="dhalfcauchy", dbeta_LnMass="dnorm",
                                   dsb="fixed", dk="fixed", dtheta="dnorm"), 
-                       param=list(dalpha=list(scale=0.1), dsig2=list(scale=0.1),
-                                  dbeta_LnMass=list(mean=0.1, sd=1),
+                       param=list(dalpha=list(meanlog=-3.5, sdlog=2.5), dsig2=list(scale=0.1),
+                                  dbeta_LnMass=list(mean=0.1, sd=0.25),
                                   dtheta=list(mean=0, sd=2.5)),
                        fixed=list(k=0, sb=numeric(0))
 )
 
 prior.N1 <- make.prior(tree, plot.prior = FALSE, 
-                       dists=list(dalpha="dhalfcauchy", dsig2="dhalfcauchy", dbeta_LnMass="dnorm",
+                       dists=list(dalpha="dlnorm", dsig2="dhalfcauchy", dbeta_LnMass="dnorm",
                                   dsb="dsb", dk="cdpois", dtheta="dnorm"), 
-                       param=list(dalpha=list(scale=0.1), dsig2=list(scale=0.1),
-                                  dbeta_LnMass=list(mean=0.1, sd=1),
+                       param=list(dalpha=list(meanlog=-3.5, sdlog=2.5), dsig2=list(scale=0.1),
+                                  dbeta_LnMass=list(mean=0.1, sd=0.25),
                                   dk=list(lambda=17.35, kmax=69.4),
                                   dtheta=list(mean=0, sd=2.5))
 )
 
 
 prior.NN <- make.prior(tree, plot.prior = FALSE, 
-                       dists=list(dalpha="dhalfcauchy", dsig2="dhalfcauchy", dbeta_LnMass="dnorm",
+                       dists=list(dalpha="dlnorm", dsig2="dhalfcauchy", dbeta_LnMass="dnorm",
                                   dsb="dsb", dk="cdpois", dtheta="dnorm"), 
-                       param=list(dalpha=list(scale=0.1), dsig2=list(scale=0.1),
-                                  dbeta_LnMass=list(mean=0.1, sd=1),
+                       param=list(dalpha=list(meanlog=-3.5, sdlog=2.5), dsig2=list(scale=0.1),
+                                  dbeta_LnMass=list(mean=0.1, sd=0.25),
                                   dk=list(lambda=17.35, kmax=69.4), 
                                   dtheta=list(mean=0, sd=2.5))
 )
@@ -56,7 +56,7 @@ model.N1 <- makeBayouModel(dat ~ LnMass, rjpars = c("theta"),
 model.NN <- makeBayouModel(dat ~ LnMass, rjpars = c("theta", "LnMass"),  
                            tree=tree, dat=dat, pred=pred, SE=MEvar, prior=prior.NN, D=DNN, slopechange="alphaweighted")
 
-gens <- 1000000
+gens <- 50000
 ## Make MCMC objects:
 mcmc.11 <- bayou.makeMCMC(tree, dat, pred=pred, SE=MEvar, model=model.11$model, prior=prior.11, samp = 100, startpar=model.11$startpar, new.dir="../output/LnMass/", outname="model11_r001", plot.freq=NULL)
 mcmc.11$run(gens/5)
@@ -69,8 +69,9 @@ startpar.N1 <- model.N1$startpar
 startpar.N1$theta[1] <- endpar.11$theta
 startpar.N1$beta_LnMass <- endpar.11$beta_LnMass
 
-mcmc.N1 <- bayou.makeMCMC(tree, dat, pred=pred, SE=MEvar, model=model.N1$model, prior=prior.N1, samp = 100, startpar=startpar.N1, new.dir="../output/LnMass/", outname="modelN1_r001", plot.freq=NULL)
-mcmc.N1$run(gens)
+#mcmc.N1 <- bayou.makeMCMC(tree, dat, pred=pred, SE=MEvar, model=model.N1$model, prior=prior.N1, samp = 100, startpar=startpar.N1, new.dir="../output/LnMass/", outname="modelN1_r001", plot.freq=NULL)
+mcmc.N1 <- readRDS("../output/LnMass/mcmc.N1.r001.rds")
+#mcmc.N1$run(gens)
 chain.N1 <- set.burnin(mcmc.N1$load(), 0.3)
 saveRDS(chain.N1, file="../output/LnMass/chain.N1.r001.rds")
 saveRDS(mcmc.N1, file="../output/LnMass/mcmc.N1.r001.rds")
@@ -79,7 +80,7 @@ endpar.N1 <- pull.pars(length(chain.N1$gen), chain.N1, model=model.N1$model)
 startpar.NN <- endpar.N1
 startpar.NN$beta_LnMass <- rep(startpar.NN$beta_LnMass, startpar.NN$ntheta)
 
-mcmc.NN <- bayou.makeMCMC(tree, dat, pred=pred, SE=MEvar, model=model.NN$model, prior=prior.NN, samp = 100, startpar=startpar.NN, new.dir="../output/LnMass/", outname="modelNN_r001", plot.freq=NULL)
+mcmc.NN <- bayou.makeMCMC(tree, dat, pred=pred, SE=MEvar, model=model.NN$model, prior=prior.NN, samp = 100, startpar=startpar.NN, new.dir="../output/LnMass/", outname="modelNN_lnorm_r004", plot.freq=500)
 mcmc.NN$run(gens)
 chain.NN <- set.burnin(mcmc.NN$load(), 0.3)
 saveRDS(chain.NN, file="../output/LnMass/chain.NN.r001.rds")
@@ -113,5 +114,5 @@ plotShiftSummaries(shiftsum.N1, pal=viridis::viridis)
 dev.off()
 
 pdf("../output/shiftsumNN.pdf")
-plotShiftSummaries(shiftsum.NN, pal=viridis::viridis)
+plotShiftSummaries(shiftsum.NN, pal=viridis::viridis, single.plot=TRUE)
 dev.off()
